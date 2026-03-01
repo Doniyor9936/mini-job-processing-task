97a37046b3e3f7d48a9fac766cf7ffb6620cff6d
sarvarbek7 <bakhtiyorov.sarvar2001@gmail.com>
2026-03-01 15:10:49 +0500

    initialize project with requirements.docs

---
+++ src/mock/mock.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { MockService } from "./mock.service";
+
+@Module(
+    {
+        providers: [MockService],
+        exports: [MockService],
+    }
+)
+export class MockModule {}
